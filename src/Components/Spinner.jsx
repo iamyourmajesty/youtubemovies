352195bc6f747bc71632f8c35629fdb2e59e3e5b
prince <princepandey2784@gmail.com>
2024-04-05 08:33:43 +0530

    centering spinner

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Spinner = () => {
   return (
+    <div className=' mt-0 h-screen w-11/12 mx-auto flex justify-center items-center'>
     <div className='loader'></div>
+    </div> 
   )
 }
 
